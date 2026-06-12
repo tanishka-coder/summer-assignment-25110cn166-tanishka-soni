f7e248daf8b59807fea1cd0f25128ca58202f1b4
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-12 13:39:25 +0530

    Update Q43 Write a program to Write function to check prime.java

---
+++ Day_11_Problems_and_Solutions/Q43 Write a program to Write function to check prime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Q43CheckPrime {
+    // Function to check if a number is prime
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            if (isPrime(n)) {
+                System.out.println(n + " is a prime number.");
+            } else {
+                System.out.println(n + " is not a prime number.");
+            }
+        }
+        sc.close();
+    }
+}
