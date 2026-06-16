181f22e265d80650883ee533e7fe1b32a5a4179f
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-16 20:43:45 +0530

    Update Q54 Write a program to Find frequency of an element.java

--- Day_14_Problems_and_Solutions/Q54 Write a program to Find frequency of an element.java
+++ Day_14_Problems_and_Solutions/Q54 Write a program to Find frequency of an element.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
 
+public class Q54 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int key = sc.nextInt();
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == key) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
