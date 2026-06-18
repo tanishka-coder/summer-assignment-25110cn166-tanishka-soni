16dffa66270be7095d20e39cc287f122c169d5b7
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-18 21:19:11 +0530

    Update Q65 Write a program to Merge arrays.java

---
+++ Day_17_Problems_and_Solutions/Q65 Write a program to Merge arrays.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {4, 5, 6};
+
+        int[] merged = new int[arr1.length + arr2.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            merged[i] = arr1[i];
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            merged[arr1.length + i] = arr2[i];
+        }
+
+        for (int x : merged) {
+            System.out.print(x + " ");
+        }
+    }
+}
