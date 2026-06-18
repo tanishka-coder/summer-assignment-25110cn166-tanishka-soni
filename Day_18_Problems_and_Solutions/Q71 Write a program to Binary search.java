2869abb5337e5368d9f087ff02606b4f2ded4591
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-18 21:17:39 +0530

    Update Q71 Write a program to Binary search.java

---
+++ Day_18_Problems_and_Solutions/Q71 Write a program to Binary search.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int key = 30;
+
+        int result = binarySearch(arr, key);
+
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
