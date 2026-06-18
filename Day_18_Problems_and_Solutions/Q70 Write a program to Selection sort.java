29ab14f8e28d52944e98cc9f9118ee2b7a7a4b2d
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-18 21:17:19 +0530

    Update Q70 Write a program to Selection sort.java

---
+++ Day_18_Problems_and_Solutions/Q70 Write a program to Selection sort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+
+        selectionSort(arr);
+
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
