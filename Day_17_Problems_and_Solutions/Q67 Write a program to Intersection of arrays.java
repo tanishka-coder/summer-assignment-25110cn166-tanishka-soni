0a3a1a99296d9915b65e0d17af125aa6ff2da2e4
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-18 21:19:48 +0530

    Update Q67 Write a program to Intersection of arrays.java

---
+++ Day_17_Problems_and_Solutions/Q67 Write a program to Intersection of arrays.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {3, 4, 5, 6};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int x : arr1) {
+            set.add(x);
+        }
+
+        for (int x : arr2) {
+            if (set.contains(x)) {
+                System.out.print(x + " ");
+            }
+        }
+    }
+}
