f5f2c7a964f211db5ab7cbfbfff0c8c0e4513014
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-24 22:36:08 +0530

    Update Q96 Write a program to Remove duplicate characters.java

---
+++ Day_24_Problems_and_Solutions/Q96 Write a program to Remove duplicate characters.java
@@ -0,0 +1,16 @@
+public class Q96_RemoveDuplicateCharacters {
+    public static void main(String[] args) {
+        String str = "programming";
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (result.indexOf(ch) == -1) {
+                result += ch;
+            }
+        }
+
+        System.out.println("After Removing Duplicates: " + result);
+    }
+}
