206df87b150cc0de71fc99044cd8c3a000df16f3
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-30 19:56:14 +0530

    Update Q105 Write a program to Create student record management system.java

---
+++ Day_27_Problems_and_Solutions/Q105 Write a program to Create student record management system.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class StudentRecordManagement {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Student ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Student Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Course: ");
+        String course = sc.nextLine();
+
+        System.out.print("Enter Marks: ");
+        int marks = sc.nextInt();
+
+        System.out.println("\n----- Student Record -----");
+        System.out.println("Student ID : " + id);
+        System.out.println("Name       : " + name);
+        System.out.println("Course     : " + course);
+        System.out.println("Marks      : " + marks);
+    }
+}
