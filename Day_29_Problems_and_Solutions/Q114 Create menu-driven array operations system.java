b21b101a825d1f6cbe19a60ea67ad4423dd97d2e
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-30 20:14:33 +0530

    Update Q114 Create menu-driven array operations system.java

--- Day_29_Problems_and_Solutions/Q114 Create menu-driven array operations system.java
+++ Day_29_Problems_and_Solutions/Q114 Create menu-driven array operations system.java
@@ -1,1 +1,76 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int choice;
+
+        do {
+            System.out.println("\n===== Array Menu =====");
+            System.out.println("1. Display Array");
+            System.out.println("2. Find Maximum");
+            System.out.println("3. Find Minimum");
+            System.out.println("4. Find Sum");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+
+                case 1:
+                    for (int i = 0; i < n; i++)
+                        System.out.print(arr[i] + " ");
+                    System.out.println();
+                    break;
+
+                case 2:
+                    int max = arr[0];
+                    for (int i = 1; i < n; i++) {
+                        if (arr[i] > max)
+                            max = arr[i];
+                    }
+                    System.out.println("Maximum = " + max);
+                    break;
+
+                case 3:
+                    int min = arr[0];
+                    for (int i = 1; i < n; i++) {
+                        if (arr[i] < min)
+                            min = arr[i];
+                    }
+                    System.out.println("Minimum = " + min);
+                    break;
+
+                case 4:
+                    int sum = 0;
+                    for (int i = 0; i < n; i++)
+                        sum += arr[i];
+
+                    System.out.println("Sum = " + sum);
+                    break;
+
+                case 5:
+                    System.out.println("Exit");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
+
+        } while (choice != 5);
+    }
+}
