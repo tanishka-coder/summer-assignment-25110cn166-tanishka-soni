c61a618d92ec4a8d4f2688498fe0241b955a7a4c
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-30 20:15:46 +0530

    Update Q117 Create student record system using arrays & strings.java

--- Day_30_Problems_and_Solutions/Q117 Create student record system using arrays & strings.java
+++ Day_30_Problems_and_Solutions/Q117 Create student record system using arrays & strings.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String[] name = new String[n];
+        int[] marks = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter Name: ");
+            name[i] = sc.nextLine();
+
+            System.out.print("Enter Marks: ");
+            marks[i] = sc.nextInt();
+            sc.nextLine();
+        }
+
+        System.out.println("\n----- Student Records -----");
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Name : " + name[i]);
+            System.out.println("Marks: " + marks[i]);
+            System.out.println();
+        }
+    }
+}
