e992bb55343bbe3ea43a62b2b782c06b5c7f2fb0
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-30 20:14:13 +0530

    Update Q113 Write a program to Create menu-driven calculator.java

--- Day_29_Problems_and_Solutions/Q113 Write a program to Create menu-driven calculator.java
+++ Day_29_Problems_and_Solutions/Q113 Write a program to Create menu-driven calculator.java
@@ -1,1 +1,63 @@
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int choice;
+        double a, b;
+
+        do {
+            System.out.println("\n===== Calculator =====");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+            if (choice >= 1 && choice <= 4) {
+                System.out.print("Enter first number: ");
+                a = sc.nextDouble();
+
+                System.out.print("Enter second number: ");
+                b = sc.nextDouble();
+            } else {
+                a = b = 0;
+            }
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Result = " + (a + b));
+                    break;
+
+                case 2:
+                    System.out.println("Result = " + (a - b));
+                    break;
+
+                case 3:
+                    System.out.println("Result = " + (a * b));
+                    break;
+
+                case 4:
+                    if (b != 0)
+                        System.out.println("Result = " + (a / b));
+                    else
+                        System.out.println("Division by zero not possible.");
+                    break;
+
+                case 5:
+                    System.out.println("Thank You!");
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice");
+            }
+
+        } while (choice != 5);
+    }
+}
