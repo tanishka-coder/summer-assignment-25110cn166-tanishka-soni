5d4961adcb3ca7e5a69ac9c0fcbea58f1e2b8b09
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-30 19:54:45 +0530

    Update Q101 Write a program to Create number guessing game.java

---
+++ Day_26_Problems_and_Solutions/Q101 Write a program to Create number guessing game.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
+
+        int number = rand.nextInt(100) + 1;
+        int guess;
+
+        System.out.println("Guess a number between 1 and 100");
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+
+            if (guess > number) {
+                System.out.println("Too High!");
+            } else if (guess < number) {
+                System.out.println("Too Low!");
+            } else {
+                System.out.println("Congratulations! You guessed correctly.");
+            }
+
+        } while (guess != number);
+    }
+}
