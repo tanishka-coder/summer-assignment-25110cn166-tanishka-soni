56a7f4d526b24dec952a1053d32b1ee71ba9e67a
Tanishka Soni <tanishkasoni2025@gmail.com>
2026-06-30 20:15:12 +0530

    Update Q116 Create inventory management system.java

--- Day_29_Problems_and_Solutions/Q116 Create inventory management system.java
+++ Day_29_Problems_and_Solutions/Q116 Create inventory management system.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String itemName;
+        int quantity;
+        double price;
+
+        System.out.print("Enter Item Name: ");
+        itemName = sc.nextLine();
+
+        System.out.print("Enter Quantity: ");
+        quantity = sc.nextInt();
+
+        System.out.print("Enter Price per Item: ");
+        price = sc.nextDouble();
+
+        double total = quantity * price;
+
+        System.out.println("\n===== Inventory Details =====");
+        System.out.println("Item Name : " + itemName);
+        System.out.println("Quantity  : " + quantity);
+        System.out.println("Price     : " + price);
+        System.out.println("Total Cost: " + total);
+    }
+}
